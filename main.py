7a57c6ecb7504b26cc2aa9ef33f0cce5f176b3c4
Wooyceps <mwooycik@gmail.com>
2024-07-19 14:00:36 +0200

    enhanced logging and added moving the csv

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support import expected_conditions
 import time
 from datetime import datetime, timedelta
 from easygui import *
-from os import listdir, getcwd
+from os import listdir, getcwd, replace
 from os.path import isfile, join, splitext, getctime
 
 SERVICE = Service(executable_path="chromeDRIVER.exe")
@@ -17,7 +17,6 @@ choices = {}
 
 def gui_and_log():
     is_log = 'LOG.txt' in listdir(getcwd())
-
     if is_log: # istnieje LOG.txt
         with open('LOG.txt', "r") as file:
             lines = [line.rstrip().split() for line in file]
@@ -26,43 +25,29 @@ def gui_and_log():
                 choices.update({line[0].rstrip(':'): f"{line[1]} {line[2]}"})
             else:
                 choices.update({line[0].rstrip(':'): line[1]})
-
         title = "MODYFIKACJE"
         message = "Czy chcesz zmodyfikować ostatnie dane wejściowe?"
         yes_no = ["TAK", "NIE"]
         output = ynbox(message, title, yes_no)
-
         if output: # chce modyfikowac - gui z defaultowymi wartosciami
             gui_settings(True)
         else: # nie chce modyfikowac
-            print("choices bez modyfikacji ", choices)
-
+            pass
     else: # nie znaleziono LOG.txt
         gui_settings(False)
-        with open('LOG.txt', "w") as file:
-            for k, v in choices.items():
-                file.write(f"{k}: {v}\n")
 
 
 def gui_settings(log_exists):
     title = "USTAWIENIA"
     text = "Wprowadź:"
     input_list = [
-        "Ścieżka do pobranych (/)",
-        "Tyska baza danych (np. FAAR, BMW)",
-        "Początek (yyyy-mm-dd hh:mm)",
-        "Koniec   (yyyy-mm-dd hh:mm)",
-        "Linia",
-        "Grupa"
+        "Ścieżka do pobranych (/)", "Tyska baza danych (np. FAAR, BMW)", "Początek (yyyy-mm-dd hh:mm)",
+        "Koniec   (yyyy-mm-dd hh:mm)", "Linia", "Grupa"
     ]
     if log_exists:
         default_list = [
-            choices["dow_path"],
-            choices["baza_d"],
-            choices["start"],
-            choices["koniec"],
-            choices["linia"],
-            choices["grupa"]
+            choices["dow_path"], choices["baza_d"], choices["start"],
+            choices["koniec"], choices["linia"], choices["grupa"]
         ]
         elements = multenterbox(text, title, input_list, default_list)
         for i, (k, v) in enumerate(choices.items()):
@@ -75,6 +60,9 @@ def gui_settings(log_exists):
         for i, key in enumerate(dir_keys):
             choices.update({key: elements[i]})
         print("choices z gui (loga nie ma) ", choices)
+    with open('LOG.txt', "w") as file:
+        for k, v in choices.items():
+            file.write(f"{k}: {v}\n")
 
 
 def get_present_element(locator, loc_text, w_time=15):
@@ -130,12 +118,18 @@ def select_group(group_name):
     group_search.send_keys(group_name + Keys.ENTER)
 
 
-def select_csv_and_download():
+def select_csv_download_move():
     export_csv = get_present_element(By.XPATH, '//*[@id="inputParams"]/table/tbody/tr[11]/td[2]/label[2]/input')
     export_csv.click()
 
     start_search = get_present_element(By.XPATH, '//*[@id="bt_Search"]')
     start_search.click()
+
+    download_path = "C:/Users/CZZ2RE/Downloads"
+    only_csvs = [f for f in listdir(download_path) if isfile(join(download_path, f)) and splitext(f)[1] == ".csv"]
+    times = [getctime(join(download_path, csv)) for csv in only_csvs]
+    latest = only_csvs[times.index(max(times))]
+    replace(join(download_path, latest), join(getcwd(), latest))
 
 
 if __name__ == "__main__":
@@ -150,8 +144,7 @@ if __name__ == "__main__":
     select_line(choices["linia"])
 
     select_group(choices["grupa"])
-
-    select_csv_and_download()
+    select_csv_download_move()
 
     time.sleep(10)
     DRIVER.quit()
